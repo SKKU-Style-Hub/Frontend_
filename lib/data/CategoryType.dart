fd840fcdb23e747dabe25a8f4902f038e61bbbd6
minhee <56909923+mingmingc2@users.noreply.github.com>
2021-04-04 21:02:03 +0900

    category

--- lib/data/CategoryType.dart
+++ lib/data/CategoryType.dart
@@ -44,39 +44,41 @@ int categoryToType(String category) {
 
 int categoryToProductType(String category) {
   //상의
-  if (category == "탑" ||
-      category == "블라우스" ||
-      category == "니트웨어" ||
-      category == "셔츠" ||
-      category == "베스트" ||
-      category == "상의") {
+  if (category.contains("탑") ||
+      category.contains("블라우스") ||
+      category.contains("니트웨어") ||
+      category.contains("셔츠") ||
+      category.contains("베스트") ||
+      category.contains("상의")) {
     return 1;
   }
   //하의
-  if (category == "청바지" ||
-      category == "팬츠" ||
-      category == "스커트" ||
-      category == "하의") {
+  if (category.contains("청바지") ||
+      category.contains("팬츠") ||
+      category.contains("스커트") ||
+      category.contains("하의")) {
     return 2;
   }
   //원피스
-  if (category == "드레스" || category == "점프수트" || category == "원피스") {
+  if (category.contains("드레스") ||
+      category.contains("점프수트") ||
+      category.contains("원피스")) {
     return 3;
   }
   //아우터
-  if (category == "코트" ||
-      category == "재킷" ||
-      category == "점퍼" ||
-      category == "패딩" ||
-      category == "아우터") {
+  if (category.contains("코트") ||
+      category.contains("재킷") ||
+      category.contains("점퍼") ||
+      category.contains("패딩") ||
+      category.contains("아우터")) {
     return 4;
   }
   //신발
-  if (category == "신발") {
+  if (category.contains("신발")) {
     return 5;
   }
   //가방
-  if (category == "가방") {
+  if (category.contains("가방")) {
     return 6;
   }
   return 0;
